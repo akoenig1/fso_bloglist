34d94cc3f38987aed6cbd3b21a6cca0f89f0b1e1
Alex Koenig <alex.koenig4@gmail.com>
2021-10-04 23:58:58 -0400

    Exercise 4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -63,3 +63,72 @@ describe('total likes', () => {
     expect(result).toBe(0)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithNoBlogs = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 50,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 25,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 25,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    )
+  })
+
+  test('when list has more than one blog, equals the blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 50
+      }
+    )
+  })
+
+  test('when list has no blogs, equals no blogs', () => {
+    const result = listHelper.favoriteBlog(listWithNoBlogs)
+    expect(result).toBe('No blogs')
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,14 +4,33 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  if(blogs) {
+  if(blogs.length > 0) {
     return blogs.reduce((acc, curr) => acc + curr.likes, 0)
   } else {
     return 0
   }
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length > 0) {
+    let favorite = { likes: 0 }
+    blogs.forEach(blog => {
+      if(blog.likes > favorite.likes) favorite = blog
+    })
+    return (
+      {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+      }
+    )
+  } else {
+    return 'No blogs'
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }

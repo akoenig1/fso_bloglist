c8a3fb4f3425c9719a8b1df3fb39b13e3a6fc0c7
Alex Koenig <alex.koenig4@gmail.com>
2021-10-04 23:48:58 -0400

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if(blogs) {
+    return blogs.reduce((acc, curr) => acc + curr.likes, 0)
+  } else {
+    return 0
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }

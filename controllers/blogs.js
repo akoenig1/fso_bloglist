e089e5d4311db0329ab19666379cc3f8c2c6151f
Alex Koenig <alex.koenig4@gmail.com>
2021-10-06 22:10:02 -0400

    Exercise 4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,16 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-  const blog = new Blog(req.body)
-  const result = await blog.save()
-  res.status(201).json(result)
+  const users = await User.find({})
+  const userId = users[0].id
+  const user = await User.findById(userId)
+
+  const blog = new Blog({
+    ...req.body,
+    user: user.id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  console.log(user)
+  await user.save({ validateBeforeSave: false })
+  res.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (req, res) => {

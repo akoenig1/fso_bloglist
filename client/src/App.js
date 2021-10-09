cf8fb355745619f2f377ea3131ce873ffcaadf64
Alex Koenig <alex.koenig4@gmail.com>
2021-10-08 23:39:59 -0400

    Exercise 5.2

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedInUserJSON = window.localStorage.getItem('loggedInUser')
+    if(loggedInUserJSON) {
+      const user = JSON.parse(loggedInUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -24,6 +32,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedInUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
@@ -36,6 +48,12 @@ const App = () => {
         setMessage(null)
       }, 4000)
     }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault()
+    localStorage.removeItem('loggedInUser')
+    setUser(null)
   }
 
   if(!user) {
@@ -74,6 +92,9 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>Logged in as {user.name}</p>
+      <button onClick={handleLogout}>Logout</button>
+      <br />
+      <br />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}

b9d8240ffdd6461932d944f4a28c68e19a2b4153
Alex Koenig <alex.koenig4@gmail.com>
2021-10-11 21:06:52 -0400

    Exercise 5.14

--- client/src/components/Blog.test.js
+++ client/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('initially renders only title and author', () => {
@@ -18,4 +18,24 @@ test('initially renders only title and author', () => {
 
   expect(component.container.querySelector('hidden-view')).toBeDefined()
   expect(component.container.querySelector('expanded-view')).toBeNull()
+})
+
+test('clicking the view button displays extended view', () => {
+  const blog = {
+    title: 'Cooking Sous Viv',
+    author: 'Vivian Cao',
+    url: 'cookingsousviv.com',
+    likes: 0,
+    user: null
+  }
+
+  const component = render(
+    <Blog blog={blog} handleDeleteBlog={() => null} />
+  )
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  expect(component.container.querySelector('hidden-view')).toBeNull()
+  expect(component.container.querySelector('expanded-view')).toBeDefined()
 })
